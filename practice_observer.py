cccee414c768ea17607afd3b132bd3faea04dff3
harvy <harvy1070@gmail.com>
2024-12-04 11:36:59 +0900

    Observer 패턴 변경, owner 수정 및 코드 수정

--- practice_observer.py
+++ practice_observer.py
@@ -3,17 +3,19 @@ from abc import ABC, abstractmethod
 # 옵저버 인터페이스
 class Observer(ABC):
     @abstractmethod
-    def update(self, message: str):
+    def update(self, message: str, m_type: str):
         pass
 
 # 구체적인 옵저버들
 class Mechanic(Observer):
-    def update(self, message: str):
-        print(f"정비사의 차량 상태 확인 : {message}")
+    def update(self, message: str, m_type: str):
+        if m_type == "car_status":
+            print(f"정비사의 차량 상태 확인 : {message}")
 
 class Owner(Observer):
-    def update(self, message: str):
-        print(f"내 차의 상태 : {message}")
+    def update(self, message: str, m_type: str):
+        if m_type == 'driver_status':
+            print(f"차 주인 확인 : {message}")
 
 class Car(ABC):
     def __init__(self):
@@ -26,11 +28,11 @@ class Car(ABC):
         self.observers.append(observer)
 
     def detach(self, observer:Observer):
-        self.observers.remove(Observer)
+        self.observers.remove(observer)
 
-    def notice(self, message: str):
+    def notice(self, message: str, m_type: str):
         for ob in self.observers:
-            ob.update(message) 
+            ob.update(message, m_type)
 
     # 기존 템플릿 메서드 부분은 그대로 내비둠
     def drive_process(self, driver):
@@ -41,7 +43,7 @@ class Car(ABC):
     
     def set_driver(self, driver): 
         self.driver = driver
-        self.notice(f"{driver.name}가 운전을 시작함")
+        self.notice(f"{driver.name}가 운전을 시작함", 'driver_status')
     
     @abstractmethod
     def engine_start(self): 
@@ -53,7 +55,7 @@ class Car(ABC):
     
     def brake(self):  
         print("브레이크 밟기")
-        self.notice("차량이 정지함")
+        self.notice("차량이 정지함", 'car_status')
 
 
 class Ferrari(Car):
@@ -63,9 +65,11 @@ class Ferrari(Car):
     
     def engine_start(self):
         print("페라리 시동 걸림 ▶▶▶ 부릉부르릉")
+        self.notice("페라리가 시동이 걸렸습니다.", 'car_status')
     
     def accelerate(self):
         print("페라리 가속 ▶▶▶ 슈우우우웅")
+        self.notice("페라리가 가속중입니다.", 'car_status')
 
 class Porsche(Car):
     def __init__(self):
@@ -91,8 +95,9 @@ if __name__ == "__main__":
     
     # 옵저버 등록
     ferrari.attach(mechanic)
+    ferrari.attach(owner)
 
-    print("=== 페라리 주행 ===")
+    print("=== 페라리 주행(고급 옵션 적용) ===")
     ferrari.drive_process(driver)
-    print("\n=== 포르쉐 주행 ===")
+    print("\n=== 포르쉐 주행(옵션 없음) ===")
     porsche.drive_process(driver)
