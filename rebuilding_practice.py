73f5e9d290605f672b9538aadcd5d33ac792a929
harvy <harvy1070@gmail.com>
2024-12-06 17:16:39 +0900

    validator 추가

--- rebuilding_practice.py
+++ rebuilding_practice.py
@@ -10,8 +10,20 @@ class GetInput:
         print("n(row)과 m(col)을 3 이상의 숫자로 입력 → ", end="", flush=True)
         self.n, self.m = map(int, sys.stdin.readline().split())
 
+# n, m 값 검증(3 이상)
+class B_Validator:
+    pass
+
+# 패턴 검증(유효한 패턴)
+class P_Validator:
+    pass
+
 # n x m 크기의 보드 생성
 class BoardGen:
+    pass
+
+# 패턴 관리
+class PatternAd:
     pass
 
 # 패턴 생성(나선형)
@@ -20,10 +32,6 @@ class SpiralGen:
 
 # 패턴 생성(대각선)
 class DiagGen:
-    pass
-
-# 패턴 관리
-class PatternAd:
     pass
 
 # 출력
